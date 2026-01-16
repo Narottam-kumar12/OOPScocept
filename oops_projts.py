a2c09d2d9c70ad3e07f83a21e696505d014d7794
narattamkumar <knarottam549@gmail.com>
2026-01-16 15:20:28 +0530

    covered till chatbook project

--- oops_projts.py
+++ oops_projts.py
@@ -18,21 +18,71 @@ class chatbook:
                            """)
         
         if user_input == '1':
-            pass
+            self.signup()
         elif user_input == '2':
-            pass
+            self.signin()
 
         elif user_input == '3':
-            pass
+            self.my_post()
         
         elif user_input == '4':
 
-            pass
+            self.sendmsg()
 
         else:
-            pass
+            exit()
+    
+    def signup(self):
+        email = input("Enter your email id:")
+        pwd = input("Enter your passward:")
+        self.username = email
+        self.passward = pwd
+        print("you have signed up successfully !")
+        print("\n")
+        self.menu()
 
-#obj = chatbook()
+    def signin(self):
+        if self.username == '' and self.passward == '':
+            print("Please signup first by pressing 1 the main ma=enu")
+
+        else:
+            uname = input(f"Enter your email/username here --> ")
+            pwd = input("Enter your password here -->  ")
+            if self.username == uname and self.passward == pwd:
+                print("You have logged in successfully !")
+                self.loggedin = True
+
+            else:
+                print("Invalid credentials, please try again !")
+
+        print("\n")
+        self.menu()
+
+    def my_post(self):
+        if self.loggedin == True:
+            txt = input("Write your post here --> ")
+            print(f"Following content has been posted --> {txt}")
+
+        else:
+            print("You need to login first to post something")
+
+        print("\n")
+        self.menu()
+
+    def sendmsg(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here --> ")
+            friend = input("Whom to send the message --> ")
+            print(f"Your message has been send to {friend}")
+
+        else:
+            print("You need to first signin first to post something.. ")
+
+        print("\n")
+        self.menu()
+
+
+obj = chatbook()
 
 
 
