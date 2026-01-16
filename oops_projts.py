c5ce8f788854610128b2de45a611c18c50c4af0f
narattamkumar <knarottam549@gmail.com>
2026-01-16 14:29:41 +0530

    bsic structure of project created

---
+++ oops_projts.py
@@ -0,0 +1,41 @@
+class chatbook:
+    def __init__(self):
+        self.username = " "
+        self.passward = " "
+        self.loggedin = False
+        self.menu()
+
+    def menu(self):
+        user_input = input("""
+                           Welcome to chatbook !! How would you proceed
+                           1. Press 1 to signup
+                           2. Press 2 to signin
+                           3. press 3 to write a post
+                           4. press 4 to me
+                           ssage a friend
+                           5. Press any key to exit
+                           
+                           """)
+        
+        if user_input == '1':
+            pass
+        elif user_input == '2':
+            pass
+
+        elif user_input == '3':
+            pass
+        
+        elif user_input == '4':
+
+            pass
+
+        else:
+            pass
+
+obj = chatbook()
+
+
+
+
+        
+        
